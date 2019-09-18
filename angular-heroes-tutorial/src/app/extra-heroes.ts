8da3c40371f4cece53409c68b2efa2fd7c786d01
kmsimmons <kmsimmons19@gmail.com>
2019-09-18 10:48:27 -0700

    Angular Practice

---
+++ angular-heroes-tutorial/src/app/extra-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Not Very Strange' },
+  { id: 12, name: 'Peanut Butter Man' },
+  { id: 13, name: 'Jelly Woman' },
+  { id: 14, name: 'Hurricane' },
+  { id: 15, name: 'Kiss of Death' },
+  { id: 16, name: 'Kiss of Life' },
+  { id: 17, name: 'Mr Fix It' },
+  { id: 18, name: 'Mr Clean' },
+  { id: 19, name: 'Person' },
+  { id: 20, name: 'Superman' }
+];
